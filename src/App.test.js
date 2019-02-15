89a2371f83bc8ca615330178d1f01f2a5a13c816
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-02-15 23:37:46 +0100

    Add project content

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,66 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('should update player score', () => {
+  const appComponent = shallow(<App />);
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5,
+    },
+    {
+      name: 'Antoś',
+      score: 0,
+    }
+  ];
+  appComponent.setState({ players });
+
+  const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+  onScoreUpdate(0, 5);
+
+  const playersAfterUpdate = appComponent.state('players');
+
+  expect(playersAfterUpdate[0].score).toEqual(5);
+});
+
+it('should add new player', () => {
+  const appComponent = shallow(<App />);
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Ania');
+
+  const players = appComponent.state('players');
+
+  expect(players.length).toEqual(1);
+  expect(players[0].name).toEqual('Ania');
+  expect(players[0].score).toEqual(0);
+});
+
+it('should remove player', () => {
+  const appComponent = shallow(<App />);
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5,
+    },
+    {
+      name: 'Antoś',
+      score: 0,
+    }
+  ];
+  
+  appComponent.setState({ players });
+  
+  const onPlayerRemove = appComponent.find(PlayersList).prop('onPlayerRemove');
+  onPlayerRemove('0');
+
+  const playersAfterUpdate = appComponent.state('players');
+
+  expect(playersAfterUpdate[0].name).toEqual('Kunegunda');
 });
